ec6f6a6748e79b00dc3a9a0c0b87e9503ebd7640
Mau <stalcarm@gmail.com>
2022-06-04 15:49:24 -0300

    functional app

--- appCuentas/app.js
+++ appCuentas/app.js
@@ -6,3 +6,23 @@ const eachPays = document.querySelector("#eachPays");
 function nameAndAmount(name, amount) {
   return `${name}: $${amount}`;
 }
+let totalAmount = 0;
+let divider = 0;
+
+function addItem() {
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    if (form.name.value != "" && form.amount.value != "") {
+      totalAmount += parseInt(form.amount.value);
+      total.innerText = totalAmount;
+      const li = document.createElement("li");
+      li.classList.add("list-group-item", "item");
+      li.innerText = nameAndAmount(form.name.value, form.amount.value);
+      results.append(li);
+      form.name.value = "";
+      form.amount.value = "";
+      divider++;
+      eachPays.innerText = totalAmount / divider;
+    }
+  });
+}
